29187b0485097ea3275f93080b6b9f5c75fd6839
Jafar <mirzaraximovjafar2001@gmail.com>
2022-10-09 10:42:29 +0500

    settings index css writed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Fotter from "./Components/Fotter/Fotter";
+import Navbar from "./Components/Navbar/Navbar";
+import HomePage from "./page/home/HomePage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <main>
+        <HomePage />
+      </main>
+      <Fotter />
+    </>
+  );
 };
 
 export default App;
