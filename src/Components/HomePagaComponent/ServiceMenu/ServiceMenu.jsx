9e2e293b34637a3f97a44473ec58a7aec7ea1370
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-10-11 21:54:20 +0500

    service menu

--- src/Components/HomePagaComponent/ServiceMenu/ServiceMenu.jsx
+++ src/Components/HomePagaComponent/ServiceMenu/ServiceMenu.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import laptop from "../../../img/laptop.jpg";
+import s from "./serviceMenu.module.css";
+const services = [
+  {
+    id: 1,
+    name: "PC",
+    img: laptop,
+  },
+  {
+    id: 2,
+    name: "Laptop",
+    img: laptop,
+  },
+  {
+    id: 3,
+    name: "Konditsioner",
+    img: laptop,
+  },
+  {
+    id: 4,
+    name: "Videonabludeniya",
+    img: laptop,
+  },
+  {
+    id: 5,
+    name: "Bitoviy texnika",
+    img: laptop,
+  },
+  {
+    id: 6,
+    name: "Printer",
+    img: laptop,
+  },
+];
 
 const ServiceMenu = () => {
-  return <div>ServiceMenu</div>;
+  return (
+    <div className={s.services + " " + "container"}>
+      <h2 className={s.serviceHeader}>Our Services</h2>
+      <div className={s.servicesMenu}>
+        {services.map((service) => {
+          return (
+            <div key={service.id} className={s.serviceBox}>
+              <div className={s.imgBox}>
+                <img src={service.img} alt={service.name} />
+              </div>
+              <h5 className={s.serviceName}>{service.name}</h5>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ServiceMenu;
