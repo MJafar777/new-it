daf0536e2ae43962eaa69dfeed8181fe73d4bade
Jafar <mirzaraximovjafar2001@gmail.com>
2022-10-11 18:38:15 +0500

    hero

--- src/Components/HomePagaComponent/Hero/Hero.jsx
+++ src/Components/HomePagaComponent/Hero/Hero.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
+import s from "./hero.module.css";
+import user1 from "../../../img/hero/users/hero-user.png";
+import { NavLink } from "react-router-dom";
+
+const container = "container";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className={s.hero}>
+      <div className={s.heroChild + " " + container}>
+        <div className={s.leftCard}>
+          <h1 className={s.title}>XIZMAT KO'RSATISH MARKAZI</h1>
+          <h1 className={s.title + " " + s.span_title}>
+            TEZKOR TEXNIKLAR IXTIROSI
+          </h1>
+          <h1 className={s.title}>TOSHKENT</h1>
+          <h2 className={s.title2}>
+            Biz sizni o'rab turgan texnologiyani hayotga olib chiqamiz!
+          </h2>
+          <NavLink className={s.links} to={"/"}>
+            Batafsil
+          </NavLink>
+        </div>
+        <div className={s.rightCard}>
+          <img src={user1} alt="user1" className={s.user_hero} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
