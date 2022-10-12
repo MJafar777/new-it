5639b3d67c43051683d892468ed77fc8f35a56e6
Jafar <mirzaraximovjafar2001@gmail.com>
2022-10-12 08:24:55 +0500

    statistics

--- src/Components/HomePagaComponent/Statistics/Statistics.jsx
+++ src/Components/HomePagaComponent/Statistics/Statistics.jsx
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import s from "./statistics.module.css";
-
 const container = "container";
+
 const Statistics = () => {
   return (
-    <div className={container + " " + s.Statistics}>
-      <div className={s.status}>
-        <h1 className={s.title}>NewIt</h1>
-        <h2>Tezkor Texniklar ixtirosi platforma №1</h2>
-      </div>
-      <div className={s.status}>
-        <h1>123</h1>
-        <p>status</p>
-      </div>
-      <div className={s.status}>
-        <h1>123</h1>
-        <p>status</p>
-      </div>
-      <div className={s.status}>
-        <h1>123</h1>
-        <p>status</p>
+    <div className={s.parent}>
+      <h1 className={s.sectionName}>Bizning statistika</h1>
+      <div className={container + " " + s.Statistics}>
+        <div className={s.status}>
+          <h1 className={s.title}>NewIt</h1>
+          <h2 className={s.title2}>Tezkor Texniklar ixtirosi platformasi №1</h2>
+        </div>
+        <div className={s.status}>
+          <h1 className={s.sts}>234 034</h1>
+          <p>Xizmat ko'rsatilgan insonlar</p>
+        </div>
+        <div className={s.status}>
+          <h1 className={s.sts}>335 123</h1>
+          <p>Turli xil savdo tovarlari</p>
+        </div>
+        <div className={s.status}>
+          <h1 className={s.sts}>235 123</h1>
+          <p>Yangi e'lonlar</p>
+        </div>
       </div>
     </div>
   );
