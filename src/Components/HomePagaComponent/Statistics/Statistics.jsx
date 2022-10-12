a43367eba9de1455cc65964968b00200cf68808d
Jafar <mirzaraximovjafar2001@gmail.com>
2022-10-12 07:12:40 +0500

    status section

--- src/Components/HomePagaComponent/Statistics/Statistics.jsx
+++ src/Components/HomePagaComponent/Statistics/Statistics.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+import s from "./statistics.module.css";
+
+const container = "container";
 const Statistics = () => {
-  return <div>Statistics</div>;
+  return (
+    <div className={container + " " + s.Statistics}>
+      <div className={s.status}>
+        <h1 className={s.title}>NewIt</h1>
+        <h2>Tezkor Texniklar ixtirosi platforma №1</h2>
+      </div>
+      <div className={s.status}>
+        <h1>123</h1>
+        <p>status</p>
+      </div>
+      <div className={s.status}>
+        <h1>123</h1>
+        <p>status</p>
+      </div>
+      <div className={s.status}>
+        <h1>123</h1>
+        <p>status</p>
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;
